694f3e655aeec5f6ae2e2ba38a8ee66c38d6b2d2
Charlie Zhang <zchglong@163.com>
2016-04-12 11:13:48 +0800

    add
    add

---
+++ swift_learning_osx/swift_learning_osx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  swift_learning_osx
+//
+//  Created by 张成龙 on 16/3/30.
+//  Copyright © 2016年 Skyworth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ swift_learning_osx/swift_learning_osx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swift_learning_osx
+//
+//  Created by 张成龙 on 16/3/30.
+//  Copyright © 2016年 Skyworth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
